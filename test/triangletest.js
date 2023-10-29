f868692ad28065ab97c6f9c41c5114fea03f366f
Jo3yG <nonuser31@yahoo.com>
2023-10-28 21:02:42 -0500

    added triangle test

---
+++ test/triangletest.js
@@ -0,0 +1,17 @@
+const Triangle = require('../lib/triangle.js');
+
+describe('Triangle',() =>{
+    describe('Method', () =>{
+        it('should return the triangle string',() =>{
+            const triangle = new Triangle({
+                logoABC:'ABC',
+                textColor:'blue',
+                shapeLogo:'Triangle',
+                bgColor:'#000',
+            });
+            expect(triangle.render()).toBe(
+                `polygon points='100 0, 0 ,0 50, 00' fill='#000'/>`
+            );
+        });
+    });
+});
